692e7e926d248edf04e9947247b803c1c0adf739
Adan Felipe Medeiros <adanfm@MacBook-Air-de-Adan.local>
2018-09-11 22:32:36 -0300

    Created basic operations rest to Category

--- src/main/java/com/adanfm/cursomc/resources/CategoryResource.java
+++ src/main/java/com/adanfm/cursomc/resources/CategoryResource.java
@@ -1,13 +1,19 @@
 package com.adanfm.cursomc.resources;
 
+import java.net.URI;
+import java.util.List;
+import java.util.stream.Collectors;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
-
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 import com.adanfm.cursomc.domain.Category;
+import com.adanfm.cursomc.dto.CategoryDTO;
 import com.adanfm.cursomc.services.CategoryService;
 
 @RestController
@@ -16,10 +22,46 @@ public class CategoryResource {
 
 	@Autowired
 	private CategoryService service;
+
+	@RequestMapping(method=RequestMethod.GET)
+	public ResponseEntity<List<CategoryDTO>> findAll() {
+		List<Category> list = service.findAll();
+		List<CategoryDTO> categoryListDto = list.stream().map(obj -> new CategoryDTO(obj)).collect(Collectors.toList());
+		return ResponseEntity.ok().body(categoryListDto);
+	}
+	
+	@RequestMapping(method=RequestMethod.POST)
+	public ResponseEntity<Void> create(@RequestBody Category obj) {
+		obj = service.insert(obj);
+		URI uri = ServletUriComponentsBuilder
+				.fromCurrentRequest()
+				.path("/{id}")
+				.buildAndExpand(obj.getId())
+				.toUri();
+
+		return ResponseEntity.created(uri).build();
+	}
+	
+	@RequestMapping(value="/{id}", method=RequestMethod.PUT)
+	public ResponseEntity<Void> update(@RequestBody Category obj, @PathVariable Integer id) {
+		obj.setId(id);
+		obj = service.update(obj);
+		
+		return ResponseEntity.noContent().build();
+	}
 	
 	@RequestMapping(value="/{id}", method=RequestMethod.GET)
-	public ResponseEntity<?> find(@PathVariable Integer id) {
+	public ResponseEntity<Category> find(@PathVariable Integer id) {
 		Category obj = service.find(id);
 		return ResponseEntity.ok().body(obj);
 	}
+
+	
+	@RequestMapping(value="/{id}", method=RequestMethod.DELETE)
+	public ResponseEntity<Void> delete(@PathVariable Integer id) {
+		service.delete(id);
+		return ResponseEntity.noContent().build();
+	}
+	
+
 }
--- src/main/java/com/adanfm/cursomc/services/CategoryService.java
+++ src/main/java/com/adanfm/cursomc/services/CategoryService.java
@@ -1,12 +1,14 @@
 package com.adanfm.cursomc.services;
 
+import java.util.List;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.DataIntegrityViolationException;
 import org.springframework.stereotype.Service;
-
 import com.adanfm.cursomc.domain.Category;
 import com.adanfm.cursomc.repositories.CategoryRepository;
+import com.adanfm.cursomc.services.exceptions.DataIntegrityException;
 import com.adanfm.cursomc.services.exceptions.ObjectNotFoundException;
 
 @Service
@@ -15,9 +17,31 @@ public class CategoryService {
 	@Autowired
 	private CategoryRepository repository;
 	
-	public Category find(Integer id) {
-		Optional<Category> obj = repository.findById(id);
-		return obj.orElseThrow(() -> new ObjectNotFoundException("Objeto nao encontrado! Id: " + id + ", Tipo:" + Category.class.getName()));
+	public List<Category> findAll() {
+		return repository.findAll();
 	}
 	
+	public Category find(Integer id) {
+		Optional<Category> obj = repository.findById(id);
+		return obj.orElseThrow(() -> new ObjectNotFoundException("Object not Found! Id: " + id + ", Type:" + Category.class.getName()));
+	}
+	
+	public Category insert(Category obj) {
+		return repository.save(obj);
+	}
+	
+	public Category update(Category obj) {
+		find(obj.getId());
+		return repository.save(obj);
+	}
+	
+	public void delete(Integer id) {
+		find(id);
+		
+		try {
+			repository.deleteById(id);	
+		} catch (DataIntegrityViolationException e) {
+			throw new DataIntegrityException("This category can not be deleted because there are products assigned");
+		}
+	}	
 }
